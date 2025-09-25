287ac3fc46701d593662878d6587665881f04625
Wa Li <orz30315@gmail.com>
2025-09-25 17:54:29 +0800

    小修改

--- src/main/java/filter/AuthFilter.java
+++ src/main/java/filter/AuthFilter.java
@@ -7,13 +7,31 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import util.JWTUtil;
 
+@WebServlet("api/*")
 public class AuthFilter implements Filter {
 
 	@Override
-	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
-		// TODO Auto-generated method stub
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+		String authHeader = request.getHeader("Authoruzation");
+		if (authHeader == null || !authHeader.startsWith("Bearer")) {
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			response.getWriter().write("{error}");
+			return;
+		}
+		String token = authHeader.substring(7);
+		try {
+			Integer memId = JWTUtil.
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
 
 	}
 
